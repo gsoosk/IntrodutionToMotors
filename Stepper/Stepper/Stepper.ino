3da9c182d166ca35d54e9a6619b656f5477f8318
Yasaman <ys.jafari98@gmail.com>
2019-02-14 18:19:41 +0330

    This commit will add complete stepper motor implementation.

--- Stepper/Stepper/Stepper.ino
+++ Stepper/Stepper/Stepper.ino
@@ -1,5 +1,8 @@
 #include <Stepper.h>
 
+const int CLOCKWISE = 1;
+const int ANTI_CLOCKWISE = -1;
+
 int STEPPER_MOTOR_PIN_1 = 2;
 int STEPPER_MOTOR_PIN_2 = 3;
 int STEPPER_MOTOR_PIN_3= 4;
@@ -8,8 +11,10 @@ int CLOCKWISE_BTN = 11;
 int ANTI_CLOCKWISE_BTN = 12;
 int STOP_BTN = 13;
 int STEP = 36;  //To calculate the step we use 360 / x when x is the desired angle
+bool runMotor;
+int rotationDir;
 
-Stepper myStepper(STEP, STEPPER_MOTOR_PIN_1, STEPPER_MOTOR_PIN_2, STEPPER_MOTOR_PIN_3, STEPPER_MOTOR_PIN_4);
+Stepper myStepper(STEP, STEPPER_MOTOR_PIN_4, STEPPER_MOTOR_PIN_3, STEPPER_MOTOR_PIN_2, STEPPER_MOTOR_PIN_1);
 
 void setup() {
   // initialize digital pin DC MOTOR PIN as an output.
@@ -17,25 +22,45 @@ void setup() {
   pinMode (STEPPER_MOTOR_PIN_2, OUTPUT);
   pinMode (STEPPER_MOTOR_PIN_3, OUTPUT);
   pinMode (STEPPER_MOTOR_PIN_4, OUTPUT);
+
+  pinMode (CLOCKWISE_BTN, INPUT_PULLUP);
+  pinMode (ANTI_CLOCKWISE_BTN, INPUT_PULLUP);
+  pinMode (STOP_BTN, INPUT_PULLUP);
   // put your setup code here, to run once:
-    myStepper.setSpeed(40);
-    Serial.begin(9600);
+  myStepper.setSpeed(20);
+  Serial.begin(9600);
+  runMotor = true;
+  rotationDir = CLOCKWISE;
 }
 
 void loop() {
-   if(digitalRead(CLOCKWISE_BTN) == LOW)
+  Serial.print(runMotor);
+  Serial.print("***");
+  if(runMotor)
+  {
+    myStepper.step(rotationDir);
+  }
+  
+  if(digitalRead(CLOCKWISE_BTN) == LOW)
   {
     while(digitalRead(CLOCKWISE_BTN) == LOW)
     {}
-     Serial.println("clockwise");
-     myStepper.step(STEP);
+    runMotor = true;
+    rotationDir = CLOCKWISE;
   }
 
-     if(digitalRead(ANTI_CLOCKWISE_BTN) == LOW)
+  if(digitalRead(ANTI_CLOCKWISE_BTN) == LOW)
   {
     while(digitalRead(ANTI_CLOCKWISE_BTN) == LOW)
     {}
-     Serial.println("anti-clockwise");
-     myStepper.step(-STEP);
+    runMotor = true;
+    rotationDir = ANTI_CLOCKWISE;
+  }
+
+  if(digitalRead(STOP_BTN) == LOW)
+  {
+    while(digitalRead(STOP_BTN) == LOW)
+    {}
+    runMotor = false;
   }
 }
